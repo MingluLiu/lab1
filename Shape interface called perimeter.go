c2aa937a1736e0efefebf659b879400ecab4cd9d
MingluLiu <minglu.liu@sjsu.edu>
2015-09-16 18:27:48 -0700

    Create Shape interface called perimeter.go

---
+++ Shape interface called perimeter.go
@@ -0,0 +1,16 @@
+package main
+import ("fmt"; "math")
+import “testing”
+func TestMain(t *testing.T){
+	r:= Rectangle{length:10, width:10}
+	var s Shape
+	s = r
+	if s.perimeter() != 16{
+		t.Error(“Expected 16, got “, s.perimeter())
+	}
+	c := Circle{pi:3.14, r:5}
+	s = c
+	if s.perimeter() != 31.400002{
+		t.Error(“Expect 31.400002, got “, s.perimeter())
+	}
+}
